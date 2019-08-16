57b2364270378a29bb49610adf02bccebcd7559e
engnadasm <42079713+engnadasm@users.noreply.github.com>
2019-08-16 15:15:56 +0200

    Add files via upload

---
+++ Assembler/src/ReportError.cpp
@@ -0,0 +1,46 @@
+#include "ReportError.h"
+#include<vector>
+#include<string>
+#include<Output.h>
+#include"Program.h"
+
+using namespace std;
+
+ReportError *ReportError::instance = 0;
+
+ReportError::ReportError()
+{
+    //ctor
+}
+
+ReportError::~ReportError()
+{
+    //dtor
+}
+
+ReportError *ReportError::getInstance()
+{
+    if (instance == 0)
+    {
+        instance = new ReportError();
+    }
+    return instance;
+}
+void ReportError::addError(string errorLine)
+{
+    this->errorLines.push_back(errorLine);
+}
+void ReportError::printReport()
+{
+    Program* program = Program::getInstance();
+    Output output = program->getOutputManager();
+    output.pass2("\n\nAssembler Report:\n");
+    if (this->errorLines.size()==0)
+    {
+        output.pass2("Successfully Assembeled");
+    }
+    for (unsigned int i = 0; i < this->errorLines.size(); i++)
+    {
+        output.pass2(errorLines[i]);
+    }
+}
